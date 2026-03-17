6f8ff7ea373b23bf02f17b094d730e1d81836c44
Sergio Guadarrama <sguadarrama@tiprotec.com>
2026-03-16 21:48:50 -0600

    change ips

--- app/config.py
+++ app/config.py
@@ -8,11 +8,11 @@ class Settings(BaseSettings):
     orchestrator_mode: str = "rest"  # "rest" | "adk"
 
     # URLs de agentes
-    sentinel_url: str = "http://localhost:4001"
-    architect_url: str = "http://localhost:4002"
-    warden_url: str = "http://localhost:4003"
-    executor_url: str = "http://localhost:4004"
-    notifier_url: str = "http://localhost:4100"
+    sentinel_url: str = "http://127.0.0.1:4001"
+    architect_url: str = "http://127.0.0.1:4002"
+    warden_url: str = "http://127.0.0.1:4003"
+    executor_url: str = "http://127.0.0.1:4004"
+    notifier_url: str = "http://127.0.0.1:4100"
 
     # Google ADK (opcional)
     gemini_api_key: str = ""
